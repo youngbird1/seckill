64b87c639a30957ecde863dcd4f79199fc0475c2
chenting <84149469@qq.com>
2022-04-17 15:26:04 +0800

    fix

--- src/main/java/com/github/lyrric/controller/TestController.java
+++ src/main/java/com/github/lyrric/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.github.lyrric.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
 
     @RequestMapping("/helloWorld")
     public String helloWorld() {
+        log.info("111111111");
+        log.info("222222222");
         return "success\n";
     }
 
